b85ca91a3f3f34afc34da49de5da1f8cbbd57bc1
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 15:17:07 -0500

    Bring coordinates code up to app level so they can be shared by components and used to query for weather data

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import CurrentConditions from './components/CurrentConditions'
 import HourlyForecast from './components/HourlyForecast'
 import Logo from './components/Logo'
@@ -5,15 +6,19 @@ import Search from './components/Search'
 import WeatherDetails from './components/WeatherDetails'
 
 function App() {
+  const [coordinates, setCoordinates] = useState<{ lat: number; lon: number } | null>(
+    null,
+  )
+
   return (
     <>
       <main className="grid grid-cols-5">
         <div className="col-span-3 space-y-8">
           <Logo />
-          <CurrentConditions />
+          <CurrentConditions coordinates={coordinates} />
         </div>
         <div className="col-span-2 space-y-4">
-          <Search />
+          <Search setCoordinates={setCoordinates} />
           <WeatherDetails />
           <HourlyForecast />
         </div>
