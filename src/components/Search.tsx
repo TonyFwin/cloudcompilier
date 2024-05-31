50c12a8a3986eec04081002f133b43bb943efe68
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 15:16:45 -0500

    Update search functionality to set hardcoded coordinates for now

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,46 @@
+import { useState } from 'react'
 import { Input } from './ui/input'
+import { Button } from './ui/button'
+// import { useGetCoordinates } from '@/hooks/useFetchCoordinates'
+import { useFetchWeather } from '@/hooks/useFetchWeather'
+import { useQueryClient } from '@tanstack/react-query'
 
-export default function Search() {
+interface SearchProps {
+  setCoordinates: (coordinates: { lat: number; lon: number }) => void
+}
+
+export default function Search({ setCoordinates }: SearchProps) {
+  const [searchTerm, setSearchTerm] = useState('')
+  const queryClient = useQueryClient()
+
+  // const { data: coordinates, refetch: refetchCoordinates } =
+  //   useFetchCoordinates(searchTerm)
+
+  const search = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+
+    if (searchTerm.length < 1) return
+
+    // Dummy lat/lon for example purposes; you should fetch real coordinates based on search term
+    const lat = 43.5488256
+    const lon = -96.7307737
+
+    setCoordinates({ lat, lon })
+    queryClient.invalidateQueries({ queryKey: ['weatherData'] })
+  }
+
   return (
     <div>
-      <Input placeholder='Search'/>
+      <form className="flex w-full items-center">
+        <Input
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <Button type="submit" onClick={search}>
+          Search
+        </Button>
+      </form>
     </div>
   )
 }
