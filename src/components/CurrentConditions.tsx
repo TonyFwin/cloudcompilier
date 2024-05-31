505f5c1634a6cfcf3606b03b7ce3c911c762114e
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 15:17:27 -0500

    Get current conditions hooked up to real data

--- src/components/CurrentConditions.tsx
+++ src/components/CurrentConditions.tsx
@@ -1,14 +1,31 @@
-export default function CurrentConditions() {
+import { useFetchWeather } from '@/hooks/useFetchWeather'
+import { WeatherData } from './types/weatherData'
+import { getTimeFromOffset } from '@/utils/getTimeFromOffset'
+
+interface CurrentConditionsProps {
+  coordinates: { lat: number; lon: number } | null
+}
+
+export default function CurrentConditions({ coordinates }: CurrentConditionsProps) {
+  const { data: weatherData, isLoading, error } = useFetchWeather(coordinates)
+  // if (isLoading) return <div>Loading...</div>
+  // if (error) return <div>Error: {error.message}</div>
   return (
     <div className="flex space-x-4">
-      <div className="text-3xl">Icon</div>
+      <div className="text-3xl">
+        {weatherData && (
+          <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} />
+        )}
+      </div>
       <div className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Temperature
+        {weatherData?.main.temp}
       </div>
 
       <div className="flex flex-col space-y-0.5">
-        <div className=" text-xl font-medium lg:text-2xl">City Name</div>
-        <div className="text-sm font-light">Date and Time</div>
+        <div className=" text-xl font-medium lg:text-2xl">{weatherData?.name}</div>
+        <div className="text-sm font-light">
+          {getTimeFromOffset(weatherData?.timezone as number).toString()}
+        </div>
       </div>
     </div>
   )
