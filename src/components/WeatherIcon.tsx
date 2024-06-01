46c3ad0bd55aa99944e6fbfc6849ea575116cf1e
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 23:00:33 -0500

    Create util and component to get weather icons

---
+++ src/components/WeatherIcon.tsx
@@ -0,0 +1,36 @@
+import {
+  WiCloud,
+  WiDaySunny,
+  WiSnow,
+  WiRain,
+  WiThunderstorm,
+  WiTornado,
+  WiDayHaze,
+} from 'react-icons/wi'
+
+import { getWeatherIcon } from '@/utils/getWeatherIcon'
+
+const WeatherIcon = ({ description }: { description: string }) => {
+  const iconName = getWeatherIcon(description)
+
+  switch (iconName) {
+    case 'cloud':
+      return <WiCloud />
+    case 'daySunny':
+      return <WiDaySunny />
+    case 'snow':
+      return <WiSnow />
+    case 'rain':
+      return <WiRain />
+    case 'thunder':
+      return <WiThunderstorm />
+    case 'tornado':
+      return <WiTornado />
+    case 'haze':
+      return <WiDayHaze />
+    default:
+      return <p>Unknown Icon</p> // Handle unexpected descriptions
+  }
+}
+
+export default WeatherIcon
---
+++ src/utils/getWeatherIcon.ts
@@ -0,0 +1,26 @@
+export const getWeatherIcon = (description: string): string => {
+  const lowerCase = description.toLowerCase()
+
+  switch (lowerCase) {
+    case 'clouds':
+      return 'cloud'
+    case 'clear':
+      return 'daySunny'
+    case 'snow':
+      return 'snow'
+    case 'rain':
+    case 'drizzle':
+      return 'rain'
+    case 'thunderstorm':
+      return 'thunder'
+    default:
+      if (description.startsWith('7')) {
+        if (description.includes('tornado')) {
+          return 'tornado'
+        } else {
+          return 'haze'
+        }
+      }
+      return 'unknown'
+  }
+}
