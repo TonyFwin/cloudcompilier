7737b001f2afa7af70249075e8056353f3902bed
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 23:00:57 -0500

    Use weather icon component over img from api

--- src/components/CurrentConditions.tsx
+++ src/components/CurrentConditions.tsx
@@ -1,5 +1,5 @@
 import { useFetchWeather } from '@/hooks/useFetchWeather'
-import { WeatherData } from './types/weatherData'
+import WeatherIcon from './WeatherIcon'
 import { getTimeFromOffset } from '@/utils/getTimeFromOffset'
 
 interface CurrentConditionsProps {
@@ -13,9 +13,7 @@ export default function CurrentConditions({ coordinates }: CurrentConditionsProp
   return (
     <div className="flex space-x-4">
       <div className="text-3xl">
-        {weatherData && (
-          <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} />
-        )}
+        {weatherData && <WeatherIcon description={weatherData.weather[0].main} />}
       </div>
       <div className="text-4xl font-extrabold tracking-tight lg:text-5xl">
         {weatherData?.main.temp}
@@ -24,7 +22,7 @@ export default function CurrentConditions({ coordinates }: CurrentConditionsProp
       <div className="flex flex-col space-y-0.5">
         <div className=" text-xl font-medium lg:text-2xl">{weatherData?.name}</div>
         <div className="text-sm font-light">
-          {getTimeFromOffset(weatherData?.timezone as number)}
+          {weatherData && getTimeFromOffset(weatherData?.timezone as number)}
         </div>
       </div>
     </div>
