5e937e15d29afb29ba9dc21cd2757a3b767ef66f
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-05-31 23:00:08 -0500

    Add real data and componetize weather details

--- src/components/WeatherDetails.tsx
+++ src/components/WeatherDetails.tsx
@@ -1,37 +1,46 @@
-export default function WeatherDetails() {
+import { useFetchWeather } from '@/hooks/useFetchWeather'
+import WeatherDetail from './WeatherDetail'
+import {
+  WiThermometer,
+  WiThermometerExterior,
+  WiRaindrop,
+  WiStrongWind,
+} from 'react-icons/wi'
+
+interface WeatherDetailsProps {
+  coordinates: { lat: number; lon: number } | null
+}
+
+export default function WeatherDetails({ coordinates }: WeatherDetailsProps) {
+  const { data: weatherData, isLoading, error } = useFetchWeather(coordinates)
+
   return (
     <div className="w-full">
-      <h2>Sunny with a chance of meatballs</h2>
-      {/* Componetize below */}
-      <div className="flex w-full justify-between">
-        <p>Max</p>
-        {/* componetize */}
-        <div>
-          <p>25 degrees C</p>
-          icon
-        </div>
-      </div>
-      <div className="flex w-full justify-between">
-        <p>Min</p>
-        <div>
-          <p>25 degrees C</p>
-          icon
-        </div>
-      </div>
-      <div className="flex w-full justify-between">
-        <p>Humidity</p>
-        <div>
-          <p>25 degrees C</p>
-          icon
-        </div>
-      </div>
-      <div className="flex w-full justify-between">
-        <p>Wind</p>
-        <div>
-          <p>25 degrees C</p>
-          icon
-        </div>
-      </div>
+      <h2>{weatherData?.weather[0].description}</h2>
+      <WeatherDetail
+        description="Max"
+        value={weatherData?.main.temp_max ?? 0}
+        unit="°"
+        Icon={WiThermometer}
+      />
+      <WeatherDetail
+        description="Min"
+        value={weatherData?.main.temp_min ?? 0}
+        unit="°"
+        Icon={WiThermometerExterior}
+      />
+      <WeatherDetail
+        description="Humidity"
+        value={weatherData?.main.humidity ?? 0}
+        unit="%"
+        Icon={WiRaindrop}
+      />
+      <WeatherDetail
+        description="Wind"
+        value={weatherData?.main.temp_max ?? 0}
+        unit="km/h"
+        Icon={WiStrongWind}
+      />
     </div>
   )
 }
