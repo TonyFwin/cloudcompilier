44f66c982840b70e2665c85e4235f691d3d84f69
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-03 13:05:53 -0500

    Add count to forecast url to reduce data coming in

--- src/api/forecast.ts
+++ src/api/forecast.ts
@@ -5,7 +5,7 @@ export const fetchForecastData = async (lat: number, lon: number) => {
   const apiKey = import.meta.env.VITE_API_KEY as string
 
   const response = await axios.get(
-    `${baseUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`,
+    `${baseUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&cnt=8&appid=${apiKey}`,
   )
 
   return response.data
