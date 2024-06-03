4a8f8b9889533a106aac4d16711b172a18389a62
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-03 13:07:12 -0500

    Create a fetch forecast hook

--- src/hooks/useFetchForecast.ts
+++ src/hooks/useFetchForecast.ts
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import { WeatherData } from '@/types/weatherData'
+import { Forecast } from '@/types/forecast'
 import { fetchForecastData } from '@/api/forecast'
 
 export const useFetchForecast = (coordinates: { lat: number; lon: number } | null) => {
-  return useQuery<WeatherData, Error>({
+  return useQuery<Forecast, Error>({
     queryKey: ['forecast', coordinates?.lat, coordinates?.lon],
     queryFn: () => fetchForecastData(coordinates!.lat, coordinates!.lon),
-    enabled: !!coordinates,
+    enabled: !!coordinates?.lat && !!coordinates?.lon,
   })
 }
