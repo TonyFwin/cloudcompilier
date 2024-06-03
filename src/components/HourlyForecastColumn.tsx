f3f76ec567d53e86689f7c4923b00ed45d561d5d
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-03 13:06:35 -0500

    Update to handle real data

--- src/components/HourlyForecastColumn.tsx
+++ src/components/HourlyForecastColumn.tsx
@@ -1,23 +1,23 @@
-import { IconType } from 'react-icons'
 import WeatherIcon from './WeatherIcon'
+import { timeFromDateString } from '@/utils/time.utils'
 
 interface HourlyForecastColumnProps {
   description: string
   iconDescription: string
-  time: number
+  dateString: string
   temperature: number
 }
 export default function HourlyForecastColumn({
   description,
   iconDescription,
-  time,
+  dateString,
   temperature,
 }: HourlyForecastColumnProps) {
   return (
-    <div>
+    <div className="w-full">
       <WeatherIcon description={iconDescription} />
       <div>
-        {time} {description}
+        {timeFromDateString(dateString)} {description}
       </div>
       <div>{temperature}°</div>
     </div>
--- src/components/HourlyForecastTable.tsx
+++ src/components/HourlyForecastTable.tsx
@@ -1,3 +1,4 @@
+import { useFetchForecast } from '@/hooks/useFetchForecast'
 import HourlyForecastColumn from './HourlyForecastColumn'
 import { ScrollArea, ScrollBar } from './ui/scroll-area'
 
@@ -6,18 +7,19 @@ interface HourlyForecastProps {
 }
 
 export default function HourlyForecastTable({ coordinates }: HourlyForecastProps) {
+  const { data: forecastData, isLoading, error } = useFetchForecast(coordinates)
   return (
     <div>
       <h2>Hourly Forecast</h2>
-      <ScrollArea className="h-[200px] w-[500px]">
-        {[...Array(24)].map((_, i) => {
+      <ScrollArea className="h-[500px] w-[750px]">
+        {forecastData?.list.map((el) => {
           return (
             <HourlyForecastColumn
-              key={i}
-              iconDescription={'clear'}
-              description={'sunny'}
-              time={i}
-              temperature={i}
+              key={el.dt}
+              iconDescription={el.weather[0].main}
+              description={el.weather[0].description}
+              dateString={el.dt_txt}
+              temperature={el.main.temp}
             />
           )
         })}
