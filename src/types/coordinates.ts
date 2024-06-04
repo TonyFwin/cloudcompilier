7b306528cf0583ba6e0c9be2f0ac41e3b4097adc
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-03 22:16:01 -0500

    Add type for openweathermap coordinates

--- src/types/coordinates.ts
+++ src/types/coordinates.ts
@@ -2,3 +2,12 @@ export interface Coordinates {
   lat: number
   lon: number
 }
+
+export interface OpenWeatherMapCoordinates {
+  country: string
+  lat: number
+  lon: number
+  local_names: Array<{ [locality: string]: string }>
+  name: string
+  state: string
+}
