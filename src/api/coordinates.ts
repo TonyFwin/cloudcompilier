0f7a90f2ad5a96d8ed7b6e18d7081430936178f3
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-03 22:14:42 -0500

    Return destructured data that is typed

--- src/api/coordinates.ts
+++ src/api/coordinates.ts
@@ -1,12 +1,13 @@
 import axios from 'axios'
 import { baseUrl } from '@/config'
+import { OpenWeatherMapCoordinates } from '@/types/coordinates'
 
 export const fetchCoordinates = async (cityName: string, limit: number) => {
   const apiKey = import.meta.env.VITE_API_KEY as string
 
-  const response = await axios.get(
+  const {data} = await axios.get<OpenWeatherMapCoordinates[]>(
     `${baseUrl}geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${apiKey}`,
   )
 
-  return response.data
+  return data
 }
