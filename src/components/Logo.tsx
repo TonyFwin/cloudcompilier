e92eb3c6cab5f398553c9cd080cd1a0e4d3f72e1
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-06 23:08:53 -0500

    Update styles of logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
   return (
     <div className="flex flex-row items-center">
       <WiDaySunnyOvercast className="h-12 w-12 md:h-20 md:w-20" />
-      <p className="cursor-default font-medium tracking-widest">CloudCompiler</p>
+      <p className="cursor-default text-sm font-semibold tracking-widest uppercase">cloudcompiler</p>
     </div>
   )
 }
