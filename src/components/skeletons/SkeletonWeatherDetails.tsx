4d885a54218348a19908d27c5f6c05564ac44f73
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-08 14:37:33 -0500

    Format file

--- src/components/skeletons/SkeletonWeatherDetails.tsx
+++ src/components/skeletons/SkeletonWeatherDetails.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '../ui/skeleton'
 export default function SkeletonWeatherDetails() {
   return (
     <div className="dark:border:white w-full border-b-2 border-b-black pt-4 md:pt-8">
-      <Skeleton className="h-6 w-24 mb-4" />
+      <Skeleton className="mb-4 h-6 w-24" />
       <div className="flex flex-col space-y-4 pl-2">
         {[...Array(4)].map((_, i) => {
           return (
