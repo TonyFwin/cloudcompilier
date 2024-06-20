756933ab522cf483feea78b4a22a4aa020ebe0b6
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-20 15:19:52 -0500

    Add ability to call jest-dom methods

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,12 +1,12 @@
 import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
 import { afterAll, afterEach, beforeAll } from 'vitest'
+import '@testing-library/jest-dom/vitest'
 import { cleanup } from '@testing-library/react'
 import { mockWeatherData, mockForecastData, mockCoordinatesData } from './mockData.ts'
 
 export const server = setupServer(
   http.get(/.*geo.*/, () => {
-    console.log('msw geo')
     return HttpResponse.json(mockCoordinatesData)
   }),
   http.get(/.*weather.*/, () => {
