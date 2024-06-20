39150d3ebb8230d5b48471373f30797722358696
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-20 15:44:27 -0500

    Add data test-id for skeletons

--- src/components/skeletons/SkeletonCurrentConditions.tsx
+++ src/components/skeletons/SkeletonCurrentConditions.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../ui/skeleton'
 
 export default function SkeletonCurrentConditions() {
   return (
-    <div className="flex items-center space-x-4">
+    <div data-testid="skeleton-current-conditions" className="flex items-center space-x-4">
       <Skeleton className="h-20 w-20 lg:h-24 lg:w-24 xl:h-28 xl:w-28" />
       <Skeleton className="h-20 w-40 lg:h-24" />
       <div className="flex flex-col space-y-4">
--- src/components/skeletons/SkeletonWeatherDetails.tsx
+++ src/components/skeletons/SkeletonWeatherDetails.tsx
@@ -2,7 +2,10 @@ import { Skeleton } from '../ui/skeleton'
 
 export default function SkeletonWeatherDetails() {
   return (
-    <div className="dark:border:white w-full border-b-2 border-b-black pt-4 md:pt-8">
+    <div
+      data-testid="skeleton-weather-details"
+      className="dark:border:white w-full border-b-2 border-b-black pt-4 md:pt-8"
+    >
       <Skeleton className="mb-4 h-6 w-24" />
       <div className="flex flex-col space-y-4 pl-2">
         {[...Array(4)].map((_, i) => {
