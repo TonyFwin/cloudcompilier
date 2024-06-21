6246d3a4fa0480e00479219c1ff70d2b830c0755
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-21 09:22:05 -0500

    Improve styles of dark mode toggle

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,6 +1,7 @@
 // src/components/ThemeSwitcher.js
 import { useState, useEffect } from 'react'
 import { Switch } from '@/components/ui/switch'
+import { WiMoonAltWaningGibbous2, WiSunrise } from 'react-icons/wi'
 
 const ThemeSwitcher = () => {
   const [darkMode, setDarkMode] = useState(false)
@@ -26,9 +27,21 @@ const ThemeSwitcher = () => {
     // >
     //   {darkMode ? 'Light' : 'Dark'}
     // </button>
-    <div className="relative mt-8 block h-12 w-12 font-bold md:absolute md:bottom-16 md:left-16 md:right-auto md:top-auto">
-      <Switch checked={darkMode} onCheckedChange={toggleDarkMode} id="darkmode-toggle" />
-      <label htmlFor="darkmode-toggle"> {darkMode ? 'Light' : 'Dark'}</label>
+    <div className="relative mt-8 flex h-12 w-12 items-center font-bold md:absolute md:bottom-16 md:left-16 md:right-auto md:top-auto">
+      <span className="mr-2">
+        <WiSunrise className="h-6 w-6 text-yellow-300" />
+      </span>
+      <Switch checked={darkMode} onCheckedChange={toggleDarkMode} id="darkmode-toggle">
+        <span
+          className={`
+          pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out 
+          ${darkMode ? 'translate-x-5' : 'translate-x-1'}
+        `}
+        />
+      </Switch>
+      <span className="ml-2">
+        <WiMoonAltWaningGibbous2 className="h-6 w-6 text-white" />
+      </span>
     </div>
   )
 }
