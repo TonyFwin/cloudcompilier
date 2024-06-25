5986c857c1d91595135b480dce3cad9dff6f1729
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-25 14:58:59 -0500

    Clean up some of the code

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -2,18 +2,19 @@ import { useState, useEffect } from 'react'
 import { Switch } from '@/components/ui/switch'
 import { WiMoonAltWaningGibbous2, WiSunrise } from 'react-icons/wi'
 
-const ThemeSwitcher = () => {
+export default function ThemeSwitcher() {
+  const STORAGE_KEY = 'darkmode'
   const [darkMode, setDarkMode] = useState(false)
 
   useEffect(() => {
-    const storedDarkMode = localStorage.getItem('darkMode')
+    const storedDarkMode = localStorage.getItem(STORAGE_KEY)
     const isDarkMode = storedDarkMode ? JSON.parse(storedDarkMode) : false
     setDarkMode(isDarkMode)
   }, [])
 
   useEffect(() => {
     document.documentElement.classList.toggle('dark', darkMode)
-    localStorage.setItem('darkMode', darkMode.toString())
+    localStorage.setItem(STORAGE_KEY, darkMode.toString())
   }, [darkMode])
 
   const toggleDarkMode = () => {
@@ -21,23 +22,19 @@ const ThemeSwitcher = () => {
   }
 
   return (
-    <div className="relative mt-8 flex h-12 w-12 items-center font-bold md:absolute md:bottom-16 md:left-16 md:right-auto md:top-auto">
+    <div className="flex items-center">
       <span className="mr-2">
         <WiSunrise className="h-6 w-6 text-yellow-300" />
       </span>
-      <Switch checked={darkMode} onCheckedChange={toggleDarkMode} id="darkmode-toggle">
-        <span
-          className={`
-          pointer-events-none inline-block h-[20px] w-[20px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out 
-          ${darkMode ? 'translate-x-5' : 'translate-x-1'}
-        `}
-        />
-      </Switch>
+      <Switch
+        className="transition duration-200 ease-in-out data-[state=unchecked]:bg-black"
+        checked={darkMode}
+        onCheckedChange={toggleDarkMode}
+        id="darkmode-toggle"
+      />
       <span className="ml-2">
         <WiMoonAltWaningGibbous2 className="h-6 w-6 text-white" />
       </span>
     </div>
   )
 }
-
-export default ThemeSwitcher
