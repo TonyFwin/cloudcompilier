49b566b5473226ba151b87b6374768d78ec97526
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-06-25 14:58:33 -0500

    Update api functions to take in a unit of measurement

--- src/api/forecast.ts
+++ src/api/forecast.ts
@@ -1,10 +1,14 @@
 import axiosInstance from './axiosInstance'
 import { baseUrl } from '@/config'
 import { ForecastData } from '@/types/forecast'
+import { UnitOfMeasurement } from '@/types/measurement'
 
-export const fetchForecastData = async (lat: number, lon: number) => {
+export const fetchForecastData = async (
+  lat: number,
+  lon: number,
+  unit: UnitOfMeasurement,
+) => {
   const apiKey = import.meta.env.VITE_API_KEY as string
-  const unit = import.meta.env.VITE_DEFAULT_UNIT_OF_MEASUREMENT ?? 'metric'
   const count = import.meta.env.VITE_DEFAULT_ ?? 8
 
   const { data } = await axiosInstance.get<ForecastData>(
--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,10 +1,14 @@
 import axiosInstance from './axiosInstance'
 import { baseUrl } from '@/config'
+import { UnitOfMeasurement } from '@/types/measurement'
 import { WeatherData } from '@/types/weather'
 
-export const fetchWeatherData = async (lat: number, lon: number) => {
+export const fetchWeatherData = async (
+  lat: number,
+  lon: number,
+  unit: UnitOfMeasurement,
+) => {
   const apiKey = import.meta.env.VITE_API_KEY as string
-  const unit = import.meta.env.VITE_DEFAULT_UNIT_OF_MEASUREMENT ?? 'metric'
 
   const { data } = await axiosInstance.get<WeatherData>(
     `${baseUrl}/data/2.5/weather?lat=${lat}&lon=${lon}&units=${unit}&appid=${apiKey}`,
