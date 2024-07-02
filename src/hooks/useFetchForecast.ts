b7d1f31ce286e02ef918004e6c2113d15316aba0
Tony Nguyen <tony.nguyen@modcreativeinc.com>
2024-07-02 14:22:04 -0500

    Update hooks to use Coordinates type

--- src/hooks/useFetchForecast.ts
+++ src/hooks/useFetchForecast.ts
@@ -1,11 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchForecastData } from '@/api/forecast'
 import { UnitOfMeasurement } from '@/types/measurement'
+import { Coordinates } from '@/types/coordinates'
 
-export const useFetchForecast = (
-  coordinates: { lat: number; lon: number },
-  unit: UnitOfMeasurement,
-) => {
+export const useFetchForecast = (coordinates: Coordinates, unit: UnitOfMeasurement) => {
   return useQuery({
     queryKey: ['forecast', coordinates?.lat, coordinates?.lon, unit],
     queryFn: () => fetchForecastData(coordinates!.lat, coordinates!.lon, unit),
--- src/hooks/useFetchWeather.ts
+++ src/hooks/useFetchWeather.ts
@@ -1,11 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchWeatherData } from '@/api/weather'
 import { UnitOfMeasurement } from '@/types/measurement'
+import { Coordinates } from '@/types/coordinates'
 
-export const useFetchWeather = (
-  coordinates: { lat: number; lon: number },
-  unit: UnitOfMeasurement,
-) => {
+export const useFetchWeather = (coordinates: Coordinates, unit: UnitOfMeasurement) => {
   return useQuery({
     queryKey: ['weatherData', coordinates.lat, coordinates.lon, unit],
     queryFn: () => fetchWeatherData(coordinates.lat, coordinates.lon, unit),
